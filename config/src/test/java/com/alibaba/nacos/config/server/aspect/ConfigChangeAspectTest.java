899fa8fc4a939f4bc83926ac1832e620315a0665
heyoulin <heyoulin@gmail.com>
2024-01-08 15:17:33 +0800

    update

--- config/src/test/java/com/alibaba/nacos/config/server/aspect/ConfigChangeAspectTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/aspect/ConfigChangeAspectTest.java
@@ -44,8 +44,8 @@ import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.Properties;
 
--- config/src/test/java/com/alibaba/nacos/config/server/service/LongPollingServiceTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/service/LongPollingServiceTest.java
@@ -36,9 +36,9 @@ import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.servlet.AsyncContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
