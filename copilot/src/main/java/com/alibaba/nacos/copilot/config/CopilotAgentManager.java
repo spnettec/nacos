458e8dc8b6d850c8f6c4419c0da5ae0f095f05df
heyoulin <heyoulin@gmail.com>
2026-04-07 15:49:28 +0800

    3.0.2.B

--- copilot/src/main/java/com/alibaba/nacos/copilot/config/CopilotAgentManager.java
+++ copilot/src/main/java/com/alibaba/nacos/copilot/config/CopilotAgentManager.java
@@ -26,7 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
